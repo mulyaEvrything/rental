0e7154d543a1c23d86a11f8f9e8231577f6d3abf
mulyaEvrything <E020321045@mahasiswa.poliban.ac.id>
2023-01-06 10:37:44 -0600

    Format beautiful

--- admin_list.php
+++ admin_list.php
@@ -87,16 +87,16 @@
       <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <!-- Add icons to the links using the .nav-icon class
-               with font-awesome or any other icon font library -->
-               <li class="nav-item active">
+          with font-awesome or any other icon font library -->
+            <li class="nav-item active">
             <a href="" class="nav-link active">
               <i class="nav-icon fas fa-tachometer-alt"></i>
               <p>
                 Dashboard
               </p>
             </a>
-          </li>
-          <li class="nav-item">
+            </li>
+            <li class="nav-item">
             <a href="#" class="nav-link">
               <i class="nav-icon fas fa-copy"></i>
               <p>
@@ -239,7 +239,7 @@
               
                         
                       </tr>
-                   <?php } ?>
+                  <?php } ?>
                 </table>
               </div>
             </div>
